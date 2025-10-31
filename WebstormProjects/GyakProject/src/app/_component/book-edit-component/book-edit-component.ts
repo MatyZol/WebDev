19f94f32ea644a2dda210059453db1cfaa4f7730
Zoltan Matyas <matyaszoltan100@gmail.com>
2025-10-31 10:21:35 +0100

    Practice #4

---
+++ WebstormProjects/GyakProject/src/app/_component/book-edit-component/book-edit-component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {Book} from '../../_model/book';
+import {BookClient} from '../../_service/book-client';
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+
+
+@Component({
+  selector: 'app-book-edit-component',
+  imports: [
+    FormsModule,
+    JsonPipe
+  ],
+  templateUrl: './book-edit-component.html',
+  styleUrl: './book-edit-component.scss'
+})
+export class BookEditComponent implements OnInit{
+
+  protected book!: Book;
+  protected param!: string | null;
+  constructor(
+    private client:BookClient,
+    private route:ActivatedRoute
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params =>
+    {
+      this.param=params.get('isbn');
+      if(params.get('isbn') == 'create'){
+        this.book = {} as Book;
+
+      }
+      else {
+        this.client.getOne(params.get('isbn')!).subscribe(book => {
+          this.book = book;
+        })
+      }
+    })
+  }
+
+
+}
--- WebstormProjects/GyakProject/src/app/app.routes.ts
+++ WebstormProjects/GyakProject/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import {BookListComponent} from './_component/book-list-component/book-list-component';
 import {AuthorListComponent} from './_component/author-list-component/author-list-component';
+import {BookEditComponent} from './_component/book-edit-component/book-edit-component';
 
 
 export const routes: Routes = [
   {path:'books',component:BookListComponent},
-  {path:'authors',component:AuthorListComponent}
+  {path:'authors',component:AuthorListComponent},
+  {path:'books/:isbn',component:BookEditComponent},
 ];
